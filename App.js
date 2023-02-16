cfd42da468a77bbb8ca4a0d5dc7713b95d01f7c0
sabirkhaloufi <sabirkhaloufi@gmail.com>
2023-02-16 14:14:09 +0100

    add react navigation

---
+++ App.js
@@ -0,0 +1,20 @@
+import {Text, StyleSheet, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Home from './src/screens/Home';
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return (
+   <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
